dce07b7a1dfda445acdbeded3c4b7e4218829b0f
AdityaGaikwad03 <gaikwadaditya0309@gmail.com>
2025-12-19 21:41:07 +0530

    solved

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,21 @@
-import Tooltip from "./Tooltip";
 import React from "react";
+import Tooltip from "./Tooltip";
 import "./App.css";
 
-export default function App() {
+const App = () => {
   return (
     <div>
       <Tooltip text="This is a tooltip">
-        <h2>Hover over me</h2>
+        <h1>Hover over me</h1>
       </Tooltip>
-      <div style={{ margin: "16px 0" }} />
+      <hr />
+
       <Tooltip text="This is another tooltip">
         <p>Hover over me to see another tooltip</p>
       </Tooltip>
+      <hr />
     </div>
   );
-}
+};
+
+export default App;
--- src/components/Tooltip.js
+++ src/components/Tooltip.js
@@ -1,23 +1,18 @@
 import React, { useState } from "react";
 
-export default function Tooltip({ text, children }) {
+const Tooltip = ({ text, children }) => {
   const [show, setShow] = useState(false);
 
-  // ensure exactly ONE React element
-  if (!React.isValidElement(children)) return null;
-
-  return React.cloneElement(
-    children,
-    {
-        className: `${children.props.className || ""} tooltip`,
-      onMouseEnter: () => setShow(true),
-      onMouseLeave: () => setShow(false),
-      onMouseOver: () => setShow(true),
-      onMouseOut: () => setShow(false),
-    },
-    <>
-      {children.props.children}
-      {show && <div className="tooltiptext">{text}</div>}
-    </>
+  return (
+    <div
+      className="tooltip"
+      onMouseEnter={() => setShow(true)}
+      onMouseLeave={() => setShow(false)}
+    >
+      {children}
+      {show && <h1 className="tooltiptext">{text}</h1>}
+    </div>
   );
-}
+};
+
+export default Tooltip;
